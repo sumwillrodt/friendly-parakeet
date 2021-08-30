7d8049dcf3d348e7748113f3806787c163a972b1
swillrodt <sumnerwillrodt@gmail.com>
2021-08-29 22:20:23 -0700

    generate random password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,62 @@
 // Assignment code here
 
+var passwordPool = "";
+
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "?@!#$%^&*'()_-+=:;<>[]\/`~||";
+
+
 
 var generatePassword = function() {
 
   // Ask for password length
   passwordText.length();
   
+  // Ask to include lowercase
+  var confirmLowercase = window.confirm("Would you like to include lowercase letters?");
+
+    if (confirmLowercase) {
+      passwordPool = passwordPool.concat(lowercaseLetters);
+      
+    } else {
+    }
+  
+  // Ask to include uppercase
+  var confirmUppercase = window.confirm("Would you like to include Uppercase letters?");
+
+    if (confirmUppercase) {
+      passwordPool = passwordPool.concat(uppercaseLetters);
+
+    } else {
+    }
+
+  // Ask to include numbers
+  var confirmNumbers = window.confirm("Would you like to include numbers?");
+    if (confirmNumbers) {
+      passwordPool = passwordPool.concat(numbers);
+
+    } else {
+    }
+
+  // Ask to include symbols
+  var confirmSymbols = window.confirm("Would you like to include symbols?");
+
+    if (confirmSymbols) {
+      passwordPool = passwordPool.concat(symbols);
+
+    } else {
+    }  
+  
+  //generate password string
+  for (var i = 0; i < passwordLength; i++) {
+    password += passwordPool.charAt(Math.floor(Math.random() * passwordPool.length));
+    
+  }
+  window.alert("Your password is: " + password);
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -25,14 +75,13 @@ generateBtn.addEventListener("click", writePassword);
 
 var passwordText = {
   length: function() {
-    window.prompt("How long would you like your password? Note: passwords must be between 8 and 128 characters.");
+    passwordLength = window.prompt("How long would you like your password? Note: passwords must be between 8 and 128 characters.");
  
-    if (passwordText.length >= 8) {
-      console.log(passwordText.length);
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      console.log("The password will be " + passwordLength + " characters long.");
     } else {
       window.prompt("That number is invalid. Please enter another number for the length of your password. Note: passwords must be between 8 and 128 characters.");
 
-      passwordLength();
     }
-  },
+  } 
 }
